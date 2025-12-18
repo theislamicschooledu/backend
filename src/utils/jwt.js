df861e249f62848476dc09ccf75e113b1b4dfe0b
theislamicschooledu <theislamicschooledu@gmail.com>
2025-12-19 00:14:18 +0600

    bug fixed

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,15 +1,18 @@
 import jsonwebtoken from "jsonwebtoken";
 
-export const generateTokenAndSetCookie = (res, userId)=>{
-    const token = jsonwebtoken.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn: "7d",
-    });
+export const generateTokenAndSetCookie = (res, userId) => {
+  const token = jsonwebtoken.sign(
+    { userId },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
 
-    res.cookie("token", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,       
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
-    return token
-}
+  return token;
+};
